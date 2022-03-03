e816bf3c133d57b23d4f6725f7736f4874a22030
Rafael Varela <rafaelvrcr@gmail.com>
2022-03-03 10:08:49 -0300

    Creating H1 and H6

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,7 +5,10 @@ function Main() {
   return <Frontend>
     
     <S.Container>
-      <div>Coluna 1</div>
+      <div>
+        <h1>creating bridges between art and technology</h1>
+        <h6>I'm a front-end developer towards fullstack, with the dream of living outside Brazil with my family.</h6>
+      </div>
       <div>Coluna 2</div>
     </S.Container>
 
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -10,4 +10,18 @@ export const Container = styled.div`
     > div {
         width: 50%;
     }
+
+    h1 {
+        color: #1e255e;
+        font-size:56px;
+        font-weight:bold;    
+    }
+
+    h6 {
+        font-size: 16px;
+        color: rgba(30,37,94,0.7);
+        font-weight:normal;
+        padding-right: 150px;
+        margin-top: 20px;
+    }
 `
